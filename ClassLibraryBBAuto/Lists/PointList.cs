bee6967ea3bf7a664fbf689f99e4f374d1f6e8b8
NextStalker <pavel-max@rambler.ru>
2015-11-17 16:17:05 +0300

    Добавил классы Point и PointList

---
+++ ClassLibraryBBAuto/Lists/PointList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace ClassLibraryBBAuto
+{
+    public class PointList : MainList
+    {
+        private List<Point> list;
+        private static PointList uniqueInstance;
+
+        private PointList()
+        {
+            list = new List<Point>();
+
+            loadFromSql();
+        }
+
+        public static PointList getInstance()
+        {
+            if (uniqueInstance == null)
+                uniqueInstance = new PointList();
+
+            return uniqueInstance;
+        }
+
+        protected override void loadFromSql()
+        {
+            DataTable dt = _provider.Select("Point");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Point point = new Point(row);
+                Add(point);
+            }
+        }
+
+        public void Add(Point point)
+        {
+            if (list.Exists(item => item == point))
+                return;
+
+            list.Add(point);
+        }
+
+        public void Delete(int idPoint)
+        {
+            Point point = getItem(idPoint);
+
+            list.Remove(point);
+
+            point.Delete();
+        }
+
+        public Point getItem(int id)
+        {
+            var points = list.Where(item => item.IsEqualsID(id));
+
+            return (points.Count() > 0) ? points.First() : new Point();
+        }
+
+        public DataTable ToDataTable()
+        {
+            return CreateTable(list);
+        }
+
+        private DataTable CreateTable(List<Point> points)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("Название");
+            dt.Columns.Add("Регион");
+
+            foreach (Point point in points)
+                dt.Rows.Add(point.getRow());
+
+            return dt;
+        }
+    }
+}
---
+++ ClassLibraryBBAuto/Tables/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace ClassLibraryBBAuto
+{
+    public class Point : MainDictionary
+    {
+        private string _name;
+        private int _idRegion;
+
+        public Point()
+        {
+            _id = 0;
+            _idRegion = 0;
+            _name = string.Empty;
+        }
+
+        public Point(DataRow row)
+        {
+            int.TryParse(row[0].ToString(), out _id);
+            _name = row[1].ToString();
+            int.TryParse(row[2].ToString(), out _idRegion);
+        }
+
+        public override void Save()
+        {
+            _provider.Insert("Point", _id, _name, _idRegion);
+
+            PointList pointList = PointList.getInstance();
+            pointList.Add(this);
+        }
+
+        internal override object[] getRow()
+        {
+            Regions regions = Regions.getInstance();
+
+            return new object[] { _id, _name, regions.getItem(_idRegion) };
+        }
+    }
+}
