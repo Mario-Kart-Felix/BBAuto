57e90a2ee9c45e5e1b9539b30512f37b1a1f396b
NextStalker <pavel-max@rambler.ru>
2015-11-18 15:26:30 +0300

    Добавил форму MyPoint_AddEdit

---
+++ BBAuto/AddEdit/MyPoint_AddEdit.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace BBAuto
+{
+    partial class MyPoint_AddEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbRegion = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Название пункта:";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(12, 65);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(377, 20);
+            this.tbName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Регион:";
+            // 
+            // cbRegion
+            // 
+            this.cbRegion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbRegion.FormattingEnabled = true;
+            this.cbRegion.Location = new System.Drawing.Point(12, 25);
+            this.cbRegion.Name = "cbRegion";
+            this.cbRegion.Size = new System.Drawing.Size(209, 21);
+            this.cbRegion.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(251, 94);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(332, 94);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // MyPoint_AddEdit
+            // 
+            this.AcceptButton = this.btnSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(419, 129);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cbRegion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MyPoint_AddEdit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Пункт назначения";
+            this.Load += new System.EventHandler(this.Point_AddEdit_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbRegion;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ BBAuto/AddEdit/MyPoint_AddEdit.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ClassLibraryBBAuto;
+
+namespace BBAuto
+{
+    public partial class MyPoint_AddEdit : Form
+    {
+        private MyPoint _mypoint;
+
+        private WorkWithForm _workWithForm;
+
+        public MyPoint_AddEdit(MyPoint mypoint)
+        {
+            InitializeComponent();
+
+            _mypoint = mypoint;
+        }
+
+        private void Point_AddEdit_Load(object sender, EventArgs e)
+        {
+            loadDictionary();
+
+            FillFields();
+
+            _workWithForm = new WorkWithForm(this.Controls, btnSave, btnClose);
+            _workWithForm.SetEditMode(_mypoint.IsEqualsID(0));
+        }
+
+        private void loadDictionary()
+        {
+            Regions regions = Regions.getInstance();
+
+            DataTable dt = regions.ToDataTable();
+
+            cbRegion.DataSource = dt;
+            cbRegion.ValueMember = dt.Columns[0].ColumnName;
+            cbRegion.DisplayMember = dt.Columns[1].ColumnName;
+        }
+        
+        private void FillFields()
+        {
+            cbRegion.SelectedValue = _mypoint.RegionID;
+            tbName.Text = _mypoint.Name;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (_workWithForm.IsEditMode())
+            {
+                if (tbName.Text == string.Empty)
+                {
+                    MessageBox.Show("Введите название", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else
+                {
+                    _mypoint.Name = tbName.Text;
+                    _mypoint.RegionID = Convert.ToInt32(cbRegion.SelectedValue);
+                    _mypoint.Save();
+                }
+
+                DialogResult = System.Windows.Forms.DialogResult.OK;
+            }
+            else
+                _workWithForm.SetEditMode(true);
+        }
+    }
+}
