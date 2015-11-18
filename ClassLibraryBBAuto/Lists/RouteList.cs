8a4e38c4d7c33a953222d67367a67f69f27ae96c
NextStalker <pavel-max@rambler.ru>
2015-11-18 11:42:48 +0300

    Добавил класс Route

---
+++ ClassLibraryBBAuto/Lists/RouteList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace ClassLibraryBBAuto
+{
+    public class RouteList : MainList
+    {
+        private List<Route> list;
+        private static RouteList uniqueInstance;
+        
+        private RouteList()
+        {
+            list = new List<Route>();
+
+            loadFromSql();
+        }
+
+        public static RouteList getInstance()
+        {
+            if (uniqueInstance == null)
+                uniqueInstance = new RouteList();
+
+            return uniqueInstance;
+        }
+
+        protected override void loadFromSql()
+        {
+            DataTable dt = _provider.Select("Route");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Route route = new Route(row);
+                Add(route);
+            }
+        }
+
+        public void Add(Route route)
+        {
+            if (list.Exists(item => item == route))
+                return;
+
+            list.Add(route);
+        }
+
+        public void Delete(int idRoute)
+        {
+            Route route = getItem(idRoute);
+
+            list.Remove(route);
+
+            route.Delete();
+        }
+
+        public Route getItem(int id)
+        {
+            var routes = list.Where(item => item.IsEqualsID(id));
+
+            return (routes.Count() > 0) ? routes.First() : new Route();
+        }
+
+        public DataTable ToDataTable()
+        {
+            return CreateTable(list);
+        }
+
+        private DataTable CreateTable(List<Route> routes)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("Пункт 1");
+            dt.Columns.Add("Пункт 2");
+            dt.Columns.Add("Дистанция");
+
+            foreach (Route route in routes)
+                dt.Rows.Add(route.getRow());
+
+            return dt;
+        }
+    }
+}
---
+++ ClassLibraryBBAuto/Tables/Route.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace ClassLibraryBBAuto
+{
+    public class Route : MainDictionary
+    {
+        private int _idPoint1;
+        private int _idPoint2;
+        private int _distance;
+
+        public Route()
+        {
+            _idPoint1 = 0;
+            _idPoint2 = 0;
+            _distance = 0;
+        }
+
+        public Route(DataRow row)
+        {
+            int.TryParse(row[0].ToString(), out _id);
+            int.TryParse(row[1].ToString(), out _idPoint1);
+            int.TryParse(row[2].ToString(), out _idPoint2);
+            int.TryParse(row[3].ToString(), out _distance);
+        }
+
+        public override void Save()
+        {
+            base.Save();
+        }
+
+        internal override void Delete()
+        {
+            _provider.Delete("Route", _id);
+        }
+
+        internal override object[] getRow()
+        {
+            PointList pointList = PointList.getInstance();
+
+            return new object[] { _id, pointList.getItem(_idPoint1), pointList.getItem(_idPoint2), _distance };
+        }
+    }
+}
