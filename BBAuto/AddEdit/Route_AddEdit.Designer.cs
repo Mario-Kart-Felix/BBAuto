4aef51885d8f7530d1ad9b494bc25b1684ad5105
NextStalker <pavel-max@rambler.ru>
2015-11-18 16:57:48 +0300

    Добавил форму Route

---
+++ BBAuto/AddEdit/Route_AddEdit.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace BBAuto
+{
+    partial class Route_AddEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbRegion = new System.Windows.Forms.ComboBox();
+            this.cbMyPoint1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbMyPoint2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbDistance = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Регион:";
+            // 
+            // cbRegion
+            // 
+            this.cbRegion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbRegion.FormattingEnabled = true;
+            this.cbRegion.Location = new System.Drawing.Point(15, 25);
+            this.cbRegion.Name = "cbRegion";
+            this.cbRegion.Size = new System.Drawing.Size(276, 21);
+            this.cbRegion.TabIndex = 1;
+            this.cbRegion.SelectedIndexChanged += new System.EventHandler(this.cbRegion_SelectedIndexChanged);
+            // 
+            // cbMyPoint1
+            // 
+            this.cbMyPoint1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMyPoint1.FormattingEnabled = true;
+            this.cbMyPoint1.Location = new System.Drawing.Point(15, 65);
+            this.cbMyPoint1.Name = "cbMyPoint1";
+            this.cbMyPoint1.Size = new System.Drawing.Size(276, 21);
+            this.cbMyPoint1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пункт назначения:";
+            // 
+            // cbMyPoint2
+            // 
+            this.cbMyPoint2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMyPoint2.FormattingEnabled = true;
+            this.cbMyPoint2.Location = new System.Drawing.Point(15, 105);
+            this.cbMyPoint2.Name = "cbMyPoint2";
+            this.cbMyPoint2.Size = new System.Drawing.Size(276, 21);
+            this.cbMyPoint2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Пункт отправления:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Дистанция:";
+            // 
+            // tbDistance
+            // 
+            this.tbDistance.Location = new System.Drawing.Point(84, 136);
+            this.tbDistance.Name = "tbDistance";
+            this.tbDistance.Size = new System.Drawing.Size(47, 20);
+            this.tbDistance.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(212, 163);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(293, 163);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // Route_AddEdit
+            // 
+            this.AcceptButton = this.btnSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(380, 198);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbDistance);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbMyPoint2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbMyPoint1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbRegion);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Route_AddEdit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Маршрут";
+            this.Load += new System.EventHandler(this.Route_AddEdit_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbRegion;
+        private System.Windows.Forms.ComboBox cbMyPoint1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbMyPoint2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbDistance;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+
+    }
+}
---
+++ BBAuto/AddEdit/Route_AddEdit.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ClassLibraryBBAuto;
+
+namespace BBAuto
+{
+    public partial class Route_AddEdit : Form
+    {
+        private Route _route;
+
+        private WorkWithForm _workWithForm;
+
+        public Route_AddEdit(Route route)
+        {
+            InitializeComponent();
+
+            _route = route;
+        }
+
+        private void Route_AddEdit_Load(object sender, EventArgs e)
+        {
+            loadRegions();
+
+            FillFields();
+
+            _workWithForm = new WorkWithForm(this.Controls, btnSave, btnClose);
+            _workWithForm.SetEditMode(_route.IsEqualsID(0));
+        }
+
+        private void FillFields()
+        {
+            if (!_route.IsEqualsID(0))
+            {
+                MyPointList myPointList = MyPointList.getInstance();
+                MyPoint myPoint = myPointList.getItem(_route.MyPoint1ID);
+                cbRegion.SelectedValue = myPoint.RegionID;
+                loadPoints();
+                cbMyPoint1.SelectedValue = _route.MyPoint1ID;
+                cbMyPoint2.SelectedValue = _route.MyPoint2ID;
+                tbDistance.Text = _route.Distance;
+            }
+        }
+
+        private void loadRegions()
+        {
+            Regions regions = Regions.getInstance();
+            DataTable dt = regions.ToDataTable();
+            cbRegion.DataSource = dt;
+            cbRegion.ValueMember = dt.Columns[0].ColumnName;
+            cbRegion.DisplayMember = dt.Columns[1].ColumnName;
+        }
+
+        private void loadPoints()
+        {
+            if (cbRegion.SelectedValue == null)
+                return;
+
+            MyPointList myPointList = MyPointList.getInstance();
+            DataTable dt = myPointList.ToDataTable(Convert.ToInt32(cbRegion.SelectedValue));
+
+            cbMyPoint1.DataSource = dt;
+            cbMyPoint1.ValueMember = dt.Columns[0].ColumnName;
+            cbMyPoint1.DisplayMember = dt.Columns[1].ColumnName;
+
+            cbMyPoint2.DataSource = dt;
+            cbMyPoint2.ValueMember = dt.Columns[0].ColumnName;
+            cbMyPoint2.DisplayMember = dt.Columns[1].ColumnName;
+        }
+
+        private void cbRegion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            loadPoints();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (_workWithForm.IsEditMode())
+            {
+                if (cbRegion.SelectedValue == null)
+                {
+                    MessageBox.Show("Выберите регион", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else if (cbMyPoint1.SelectedValue == null)
+                {
+                    MessageBox.Show("Выберите пункт отправления", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else if (cbMyPoint2.SelectedValue == null)
+                {
+                    MessageBox.Show("Выберите пункт назначения", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else if (tbDistance.Text == string.Empty)
+                {
+                    MessageBox.Show("Введите дистанцию", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else
+                {
+                    _route.MyPoint1ID = Convert.ToInt32(cbMyPoint1.SelectedValue);
+                    _route.MyPoint2ID = Convert.ToInt32(cbMyPoint2.SelectedValue);
+                    _route.Distance = tbDistance.Text;
+                    _route.Save();
+                }
+
+                DialogResult = System.Windows.Forms.DialogResult.OK;
+            }
+            else
+                _workWithForm.SetEditMode(true);
+        }
+    }
+}
