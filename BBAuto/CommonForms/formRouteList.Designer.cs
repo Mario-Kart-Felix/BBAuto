c13c43ddd1042db47bfef07750fffe58ef529aa9
NextStalker <pavel-max@rambler.ru>
2015-11-19 10:58:59 +0300

    Добавил форму "formRouteList"

---
+++ BBAuto/CommonForms/formRouteList.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BBAuto.CommonForms
+{
+    partial class formRouteList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(93, 12);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 47;
+            this.btnDel.Text = "Удалить";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 46;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(408, 330);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 45;
+            this.btnCancel.Text = "Закрыть";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 41);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgv.Size = new System.Drawing.Size(471, 283);
+            this.dgv.TabIndex = 44;
+            this.dgv.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgv_CellMouseDoubleClick);
+            this.dgv.Resize += new System.EventHandler(this.dgv_Resize);
+            // 
+            // formRouteList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 365);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.dgv);
+            this.Name = "formRouteList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Справочник \"Маршруты\"";
+            this.Load += new System.EventHandler(this.formRouteList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
---
+++ BBAuto/CommonForms/formRouteList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ClassLibraryBBAuto;
+
+namespace BBAuto.CommonForms
+{
+    public partial class formRouteList : Form
+    {
+        private MainDGV _dgvMain;
+
+        private RouteList _routeList;
+
+        public formRouteList()
+        {
+            InitializeComponent();
+
+            _routeList = RouteList.getInstance();
+        }
+
+        private void formRouteList_Load(object sender, EventArgs e)
+        {
+            loadData();
+
+            _dgvMain = new MainDGV(dgv);
+
+            ResizeDGV();
+        }
+
+        private void dgv_Resize(object sender, EventArgs e)
+        {
+            ResizeDGV();
+        }
+
+        private void ResizeDGV()
+        {
+            dgv.Columns[1].Width = dgv.Width / 3;
+            dgv.Columns[2].Width = dgv.Width / 3;
+            dgv.Columns[3].Width = dgv.Width / 3;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            openAddEdit(new Route());
+        }
+        
+        private void dgv_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            openAddEdit(_routeList.getItem(_dgvMain.GetID()));
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            _routeList.Delete(_dgvMain.GetID());
+
+            loadData();
+        }
+
+        private void openAddEdit(Route route)
+        {
+            Route_AddEdit routeAE = new Route_AddEdit(route);
+            if (routeAE.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                loadData();
+        }
+
+        private void loadData()
+        {
+            dgv.DataSource = _routeList.ToDataTable();
+
+            if (dgv.Columns.Count > 0)
+                dgv.Columns[0].Visible = false;
+        }
+    }
+}
