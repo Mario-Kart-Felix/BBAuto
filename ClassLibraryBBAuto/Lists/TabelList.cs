3c55202a16dfc6db5d31e9c12ff94dc27fba8dd2
NextStalker <pavel-max@rambler.ru>
2015-11-24 17:58:34 +0300

    Добавил таблицу табель

---
+++ ClassLibraryBBAuto/Lists/TabelList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace ClassLibraryBBAuto
+{
+    public class TabelList : MainList
+    {
+        private List<Tabel> _list;
+        private static TabelList _uniqueInstance;
+
+        private TabelList()
+        {
+            _list = new List<Tabel>();
+
+            loadFromSql();
+        }
+
+        public static TabelList GetInstance()
+        {
+            if (_uniqueInstance == null)
+                _uniqueInstance = new TabelList();
+
+            return _uniqueInstance;
+        }
+
+        protected override void loadFromSql()
+        {
+            DataTable dt = _provider.Select("Tabel");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Tabel tabel = new Tabel(row);
+                Add(tabel);
+            }
+        }
+
+        public void Add(Tabel tabel)
+        {
+            if (_list.Exists(item => item == tabel))
+                return;
+
+            _list.Add(tabel);
+        }
+
+        internal List<int> GetDays(Driver driver, DateTime date)
+        {
+            var listNew = from item in _list
+                          where item.driver == driver && item.Date == date
+                          orderby item.Date.Day
+                          select item.Date.Day;
+
+            return listNew.ToList();
+        }
+    }
+}
